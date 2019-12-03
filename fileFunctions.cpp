a69eb2b579f3312260714298346224d2ceeeb5ab
sbotmtwigrm <58452919+sbotmtwigrm@users.noreply.github.com>
2019-12-03 11:13:31 -0500

    Add files via upload

--- fileFunctions.cpp
+++ fileFunctions.cpp
@@ -1,20 +1,23 @@
 #include <stdio.h>
+#include <string.h>
 #include <math.h>
 #include <allegro5/allegro.h>
 #include <allegro5/allegro_font.h>
 #include <allegro5/allegro_ttf.h>                       // For allegro, must be in compiler search path.
 #include <allegro5/allegro_native_dialog.h> 		// for message box
-#include "game.h"
+#include "monarchs.h"
 
-static const int MAX = 20;
-static const int ACCEL = 3;
-static const int NATDECEL = 3;
-static const int TURN = 0.25;
+#define MAX 20
+#define ACCEL 3
+#define NATDECEL 3
+#define TURN 0.25
 
+//Use in a while loop
 float calcSpeed(float prevSpeed, bool accelKey_down) {
+    // Declaring variable
     float speed = 0;
 
-    // Calculate speed depending on if the acceleration key is pressed
+    //Calculate return value
     if (accelKey_down) {
         if (prevSpeed < MAX - ACCEL) {
             speed = prevSpeed + ACCEL;
@@ -28,25 +31,56 @@ float calcSpeed(float prevSpeed, bool accelKey_down) {
             speed = 0;
         }
     }
-
     return speed;
 }
 
 float calcDirection(float prevDir, bool leftTurn_down, bool rightTurn_down) {
-    float slope = 0;
     float angle = 0;
+    angle = prevDir;
 
-    angle = atan(prevDir);
-
-    if (leftTurn_down && rightTurn_down) {
-        angle = angle;
-    } else if (leftTurn_down) {
+    if (leftTurn_down) {
         angle += TURN;
     } else if (rightTurn_down) {
         angle -= TURN;
     }
 
-    slope = tan(angle);
-    return slope;
+    return angle;
 }
+
+void calcMovement(float &posX, float &posY, Vehicle prev, Input key) {
+    float distance = 0;
+    float angle = 0;
+
+    distance = calcSpeed(prev.moveStats.speed, key.up);
+    angle = calcDirection(prev.moveStats.direction, key.left, key.right);
+
+    posX += distance * cos(angle);
+    posY += distance * sin(angle);
+}
+
+/*struct Input {
+    bool up;
+    bool down;
+    bool left;
+    bool right;
+};
+
+struct Movement {
+    bool onTrack;
+    float speed;
+    float direction;
+};
+
+struct Vehicle {
+    ALLEGRO_BITMAP *bitmap;
+    Movement moveStats;
+    int fuel;
+    int x;
+    int y;
+};*/
+
+
+
+
+
 
